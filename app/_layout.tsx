826bc3e67293a1ff6fd7d472174b323fe2071dfb
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-12-12 19:31:43 -0600

    feat: adds vercel analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
@@ -55,6 +56,7 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <RootLayoutNav />
+      <Analytics />
     </QueryClientProvider>
   );
 }
